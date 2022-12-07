a748d262b20f85e81aed462349d72beabea9cd10
Torsten Krüüüüuger <torsten.kruger90@gmail.com>
2022-12-07 02:48:19 +0100

    Added messageEl, cardsEl, sumEl for showing text on screen

--- scripts.js
+++ scripts.js
@@ -1,21 +1,26 @@
-let firstCard = 10;
-let secondCard = 4;
-let sum = firstCard + secondCard;
-let hasBlackJack = false;
-let isAlive = true;
-let message = "";
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.querySelector("#message-el")
+let cardsEl = document.querySelector("#cards-el")
+let sumEl = document.querySelector("#sum-el")
 
 function startGame() {
     if (sum <= 20) {
-        message = "Do you wan to draw a new card?"
+        message = "Do you want to draw a new card?"
     } 
     else if (sum === 21) {
         message = "Wohoo! You've got Blackjack!"
         hasBlackJack = true;
     }
     else {
-        message = "You're out of the game!";
-        isAlive = false;
+        message = "You're out of the game!"
+        isAlive = false
     }
-    console.log(message);
+    messageEl.textContent = message
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
 }
